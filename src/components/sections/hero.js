57c321d717505555ee8a9e0da59357a2572ead7f
Stackbit Code Editor <projects@stackbit.com>
2021-09-25 20:32:55 +0000

    hero.js: changed

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -6,13 +6,13 @@ export default function Hero(props) {
   return (
     <div className="bg-blue-900 px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
       <div className="max-w-xl sm:mx-auto lg:max-w-2xl">
-        <div className="flex flex-col mb-16 sm:text-left sm:mb-0">
+        <div className="flex flex-col mb-16 sm:text-center sm:mb-0">
           <a href="/" className="mb-6 sm:mx-auto">
-            <div className="flex items-left justify-left w-12 h-12 rounded-full bg-indigo-50">
+            <div className="flex items-center justify-center w-12 h-12 rounded-full bg-indigo-50">
               <Bolt size={10} />
             </div>
           </a>
-          <div className="max-w-xl mb-10 md:mx-auto sm:text-left lg:max-w-2xl md:mb-12">
+          <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
             {props.title && (
               <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
                 <span className="relative inline-block">
