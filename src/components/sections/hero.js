2e18324ef9002a33ac9701317a866959817a6a64
Stackbit Code Editor <projects@stackbit.com>
2021-09-25 20:30:26 +0000

    hero.js: changed

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -12,7 +12,7 @@ export default function Hero(props) {
               <Bolt size={10} />
             </div>
           </a>
-          <div className="max-w-xl mb-10 md:mx-auto sm:text-left lg:max-w-2xl md:mb-12">
+          <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
             {props.title && (
               <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
                 <span className="relative inline-block">
