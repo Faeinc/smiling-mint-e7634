8845982036a039f0bb01946ee3440b955c49fa24
Stackbit Code Editor <projects@stackbit.com>
2021-09-25 13:47:03 +0000

    hero.js: changed

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -5,7 +5,7 @@ import Dots from '../svgs/dots';
 export default function Hero(props) {
   return (
     <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-      <div className="max-w-xl sm:mx-auto lg:max-w-2xl bg-indigo-50">
+      <div className="max-w-xl sm:mx-auto lg:max-w-2xl ">
         <div className="flex flex-col mb-16 sm:text-center sm:mb-0">
           <a href="/" className="mb-6 sm:mx-auto">
             <div className="flex items-center justify-center w-12 h-12 rounded-full bg-indigo-50">
